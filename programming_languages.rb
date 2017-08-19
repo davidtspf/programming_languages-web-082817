c1c5fa9d4784e4851ba74f5e305768b4a575e2ef
davidtspf <david.tannenbaum@gmail.com>
2017-08-19 17:54:41 -0400

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,68 @@
+require 'pry'
+
 def reformat_languages(languages)
-  # your code here
+  new_hash = {}
+
+  languages.each do |style_type, language_hash|
+    ## :oo, :ruby =>
+    language_hash.each do |language_name, type_hash|
+      # ruby, :type =>
+      new_hash[language_name] ||= {}
+      type_hash.each do |type, string_value|
+        # type, type value
+        new_hash[language_name][type] = string_value
+      end
+      new_hash[language_name][:style] ||= []
+      new_hash[language_name][:style] << style_type # unless new_hash[language_name][:style].include?(style_type)
+    end
+  end
+  return new_hash
 end
+
+
+# languages = {
+#   :oo => {
+#     :ruby => {
+#       :type => "interpreted"
+#     },
+#     :javascript => {
+#       :type => "interpreted"
+#     },
+#     :python => {
+#       :type => "interpreted"
+#     },
+#     :java => {
+#       :type => "compiled"
+#     }
+#   }
+#
+#   reworked = {
+#   :ruby => {
+#     :type => "interpreted",
+#     :style => [:oo]
+#   },
+#   :javascript => {
+#     :type => "interpreted",
+#     :style => [:oo, :functional]
+#   },
+#   :python => {
+#     :type => "interpreted",
+#     :style => [:oo]
+#   }
+# }
+#
+# new_hash = {
+#   :ruby => {
+#     :type => "interpreted",
+#     :style = [oo]
+#   },
+#   :javascript => {
+#     :type => "interpreted",
+#   },
+#   :python => {
+#     :type => "interpreted",
+#   }
+#   :java => {
+#     :type => "compiled"
+#   }
+# }
